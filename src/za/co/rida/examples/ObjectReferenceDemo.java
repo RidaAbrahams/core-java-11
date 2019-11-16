9176a038e33687f0b374116d865d76307e1c2255
RidaAbrahams <rida.abrahams@gmail.com>
2019-11-16 06:20:49 +0200

    Object Reference demo added

--- src/za/co/rida/examples/ObjectReferenceDemo.java
+++ src/za/co/rida/examples/ObjectReferenceDemo.java
@@ -2,13 +2,14 @@ package za.co.rida.examples;
 
 import java.util.Date;
 
-public class Hello {
+public class ObjectReferenceDemo {
 
     public static void main(String[] args) {
-        System.out.println(new Date());
-        String s = new Date().toString();
-        System.out.println(s);
+        // Object variables, like birthday always contain a reference to an object stored on the heap.
+        // the variable birthday is not the object.
         Date birthday = new Date();
+        // you can assign object variables to already created objects as indicated below.
+        // both the deadline and birthday object variables contain a reference to the same Date object on the heap.
         Date deadline = birthday;
         String s1 = deadline.toString();
         System.out.println(birthday);
